8da9ce13f0eacb39eec2f2c1b57756d6c10bf0a8
kristenmabry <kristen@themabryfamily.com>
2021-07-01 18:56:49 -0600

    use eeprom to save and restore latest

--- ArduinoProject.ino
+++ ArduinoProject.ino
@@ -1,5 +1,6 @@
 #include <SoftwareSerial.h>
 #include <RGBmatrixPanel.h>
+#include <EEPROM.h>
 #include "Bluetooth.h"
 #include "Layout.h"
 
@@ -10,12 +11,15 @@
 #define B   A1
 #define C   A2
 
+#define TLENGTH 21
+
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 /* Create object named bt of the class SoftwareSerial */ 
 SoftwareSerial bt(11, 12); /* (Rx,Tx) */
 
 State state = State::Start;
 int counter = 0;
+int eepromAddr = 0;
 char* buffer;
 Layout layout;
 
@@ -23,36 +27,17 @@ void setup() {
   Serial.begin(9600); /* Define baud rate for serial communication */
   bt.begin(9600); /* Define baud rate for software serial communication */
   matrix.begin();
- 
-  // draw some text!
-  matrix.setCursor(1, 0);  // start at top left, with one pixel of spacing
-  matrix.setTextSize(1);   // size 1 == 8 pixels high
 
-  // print each letter with a rainbow color
-  matrix.setTextColor(matrix.Color333(7,0,0));
-  matrix.print('1');
-  matrix.setTextColor(matrix.Color333(7,4,0));
-  matrix.print('6');
-  matrix.setTextColor(matrix.Color333(7,7,0));
-  matrix.print('x');
-  matrix.setTextColor(matrix.Color333(4,7,0));
-  matrix.print('3');
-  matrix.setTextColor(matrix.Color333(0,7,0));
-  matrix.print('2');
+  buffer = new char[TLENGTH];
 
-  matrix.setCursor(1, 9);  // next line
-  matrix.setTextColor(matrix.Color333(0,7,7));
-  matrix.print('*');
-  matrix.setTextColor(matrix.Color333(0,4,7));
-  matrix.print('R');
-  matrix.setTextColor(matrix.Color333(0,0,7));
-  matrix.print('G');
-  matrix.setTextColor(matrix.Color333(4,0,7));
-  matrix.print('B');
-  matrix.setTextColor(matrix.Color333(7,0,4));
-  matrix.print('*');
-
-  buffer = new char[21];
+  for (int i = 0; i < TLENGTH; ++i)
+  {
+    buffer[i] = EEPROM.read(i);
+  }
+  if (buffer[0] == 'T')
+  {
+    layout = Layout(matrix, buffer);
+  }
 }
 
 void loop() {
@@ -61,6 +46,13 @@ void loop() {
   if (state == State::Finish)
   {
     layout = Layout(matrix, buffer);
+
+    for (int i = 0; i < TLENGTH; ++i)
+    {
+      EEPROM.write(eepromAddr, buffer[i]);
+      ++eepromAddr;
+    }
+    
     state = State::Start;
   }
 }
